5c93fd6c4258a0d36e2634358d79580c56f5be70
Karl-Magnus Jönsson <karl-magnus.jonsson@kristianstad.se>
2014-06-10 15:22:55 +0200

    #8 Print mask

--- modules/Module/SPrint/SPrint_conf.js
+++ modules/Module/SPrint/SPrint_conf.js
@@ -10,5 +10,7 @@ sMap.moduleConfig.SPrint = {
 		// default is otherwise 72.
 		DOTS_PER_INCH: 96,
 		
+		usePrintMask : true,
+		
 		printCopyrightNotice: '<div id="print-dialog-userconditions" class="ui-dialog-content ui-widget-content" scrolltop="0" scrollleft="0" style="width: auto; min-height: 0px; height: 183px;">För utdrag från kartan/flygfotot till tryck eller annan publicering, krävs tillstånd från Malmö Stadsbyggnadskontor. Vid frågor om tillstånd, användningsområden eller kartprodukter kontaktas Stadsbyggnadskontorets kartförsäljning: 040-34 24 35 eller <a href="mailto:sbk.sma@malmo.se?subject=Best%E4lla karta">sbk.sma@malmo.se</a>.<br><strong>Accepterar du villkoren?</strong></div>'
 };
