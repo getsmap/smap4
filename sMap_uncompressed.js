862c17ae163e3a9f8eb000cd8c62a7285e55c5c4
johanlahti <info@lahtitude.se>
2014-08-22 11:40:08 +0200

    blixten

--- sMap_uncompressed.js
+++ sMap_uncompressed.js
@@ -5554,7 +5554,7 @@ sMap.Lang.lang.Blixten = {
 	"sv-SE" : {
 		toolButtonText : "Sök i kartan",
 		dialogTitle: "Sök i kartan",
-		introText1: "<p>Sök i kartan” hämtar information inom en angiven cirkel. Sökningen träffar alla översiktsplanelager och riksintressen.</p>" + 
+		introText1: "<p>\"Sök i kartan\” hämtar information inom en angiven cirkel. Sökningen träffar alla översiktsplanelager och riksintressen.</p>" + 
 			"<p>Därutöver hämtas följande underlag: naturvårdsplan, naturreservat uppmärksamhetsavstånd kring farliga verksamheter, skyddsområde för grundvattentäkt, gasledning, kraftledningar.</p>" +
 			"<p>Ställ in önskad radie och klicka därefter i kartan.</p>",
 		introText2: "",
